e3143337d29cabe98d68d190b56896a3c2bbd7e8
Ceasar Cyrillus <te15ceacyr@falufri.se>
2017-05-23 08:31:02 +0200

    Added base form

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/action", methods=["POST"])
+def action():
+	msg = request.form["msg"]
+	return render_template("index.html", msg=msg)
